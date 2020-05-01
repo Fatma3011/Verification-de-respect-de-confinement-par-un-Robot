01454492ce8daaf7b989b38b224c568b8e080dcb
Fatma <fatma.chtiouigarbaa@ensi-uma.tn>
2020-05-02 00:32:52 +0200

    classe voiture

---
+++ eclipse-workspace/Groupe-4/src/Projet/Voiture.java
@@ -0,0 +1,28 @@
+package Projet;
+
+import java.util.Scanner;
+
+public class Voiture extends Objet{
+	boolean vide ;   // si vide=true la voiture est vide 
+	Homme contenu;  // si la voiture n'est pas vide le chauffeur sera ajouté 
+	Voiture(boolean v,int x,int y){
+		super(x,y);
+		if(v==true) {
+			System.out.println("Une voiture vide a été ajoutée avec succès ");
+		}
+		else{
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Veuillez saisir 'oui' si le chauffeur a une attestation de déplacement et 'non' sinon ");
+			String motif = sc.nextLine();
+			if (motif=="oui") {
+				contenu = new Homme(x,y,true);
+				System.out.println("Une voiture contenant un chauffeur ayant une attestation de déplacement a été ajoutée avec succès ");
+			}
+			else {
+				contenu = new Homme(x,y,false);
+				System.out.println("Une voiture contenant un chauffeur n'ayant pas une attestation de déplacement a été ajoutée avec succès ");
+			}
+			sc.close();
+		}
+	}
+}
